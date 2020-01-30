166af63fdab077e03a8e07e529cd4132f280be3a
ATTILA0516 <46132263+ATTILA0516@users.noreply.github.com>
2020-01-30 20:03:58 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -415,7 +415,28 @@ async def on_message(message):
                     #各ページごとに必要なリアクション
 
                 await message.channel.send(embed=open_message(got_message))
-        
+
+    GLOBAL_CH_NAME = "noa-global-chat" # グローバルチャットのチャンネル名
+    GLOBAL_WEBHOOK_NAME = "noa-webhook" # グローバルチャットのWebhook名
+
+    if message.channel.name == GLOBAL_CH_NAME:
+        # hoge-globalの名前をもつチャンネルに投稿されたので、メッセージを転送する
+        await message.delete()
+
+        channels = client.get_all_channels()
+        global_channels = [ch for ch in channels if ch.name == GLOBAL_CH_NAME]
+
+        for channel in global_channels:
+            ch_webhooks = await channel.webhooks()
+            webhook = discord.utils.get(ch_webhooks, name=GLOBAL_WEBHOOK_NAME)
+
+            if webhook is None:
+                # そのチャンネルに hoge-webhook というWebhookは無かったので無視
+                continue
+            await webhook.send(content=message.content,
+                username=message.author.name,
+                avatar_url=message.author.avatar_url_as(format="png"))
+
 def open_message(message):
     """
     メッセージを展開し、作成した埋め込みに各情報を添付し返す関数
