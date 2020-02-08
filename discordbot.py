c423eb362c192b631b6ed9671d4acd7db6e8a156
8ka1alu <46132263+8ka1alu@users.noreply.github.com>
2020-02-08 21:06:59 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -449,12 +449,12 @@ tundrach = 673412099350855702
 #ウェルカムメッセージ
 @client.event
 async def on_member_join(member):
-    if message.channel.id == tundras:
+    if message.guild.id == tundras:
         await client.get_channel(tundrach).send(f'ようこそ、**{member.name}**さん！私は{client.user.name}です。\n私の機能で分からないことがありましたら、```ヘルプ```と発言してください。')
 
 @client.event
 async def on_member_remove(member):
-    if message.channel.id == tundras:
+    if message.guild.id == tundras:
         await client.get_channel(tundrach).send(f'**{member.name}がサーバーを離れました。**')
 
 client.run(TOKEN)
