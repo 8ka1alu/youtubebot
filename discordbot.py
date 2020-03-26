b4fce935f44e7318567623b8ce8f599eabe17f9c
8ka1alu <46132263+8ka1alu@users.noreply.github.com>
2020-03-26 16:03:15 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -236,7 +236,7 @@ async def on_message(message):
         else:
             weekday_name = "エラー"
         date = datetime.datetime.now()
-        embed = discord.Embed(title="時計", description="TimeZone=Japan",color=random.choice((0,0x1abc9c,0x11806a,0x2ecc71,0x1f8b4c,0x3498db,0x206694,0x9b59b6,0x71368a,0xe91e63,0xad1457,0xf1c40f,0xc27c0e,0xe67e22,0x95a5a6,0x607d8b,0x979c9f,0x546e7a,0x7289da,0x99aab5)))
+        embed = discord.Embed(title="時計", description="TimeZone(Japan)",color=random.choice((0,0x1abc9c,0x11806a,0x2ecc71,0x1f8b4c,0x3498db,0x206694,0x9b59b6,0x71368a,0xe91e63,0xad1457,0xf1c40f,0xc27c0e,0xe67e22,0x95a5a6,0x607d8b,0x979c9f,0x546e7a,0x7289da,0x99aab5)))
         embed.add_field(name="日付", value=f'{date.year}年{date.month}月{date.day}日{weekday_name}', inline=False)
         embed.add_field(name="時間", value=f'{date.hour}時{date.minute}分{date.second}秒', inline=False)
         embed.set_thumbnail(url="https://cdn.discordapp.com/attachments/688986679956340804/690461569854996490/180half_f.gif")
