6082e800ce0d49de761ff2b926197ac0dbb19e9c
8ka1alu <46132263+8ka1alu@users.noreply.github.com>
2020-03-26 15:55:11 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -218,7 +218,7 @@ async def on_message(message):
         date = datetime.datetime.now()
         await message.channel.send(f'今は{date.hour}時{date.minute}分{date.second}秒だよ！')
     if message.content == '時計':
-        weekdays = datetime.date.weekday()
+        weekdays = datetime.date.today().weekday()
         if weekdays == 0:
             weekday_name == "月曜日"
         elif weekdays == 1:
