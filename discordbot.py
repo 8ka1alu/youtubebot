4f770fb062bafaca75d23d8b60a061c8451ee872
8ka1alu <46132263+8ka1alu@users.noreply.github.com>
2020-06-01 15:27:37 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -564,7 +564,21 @@ async def on_member_remove(member):
         color = random.choice((0,0x1abc9c,0x11806a,0x2ecc71,0x1f8b4c,0x3498db,0x206694,0x9b59b6,0x71368a,0xe91e63,0xad1457,0xf1c40f,0xc27c0e,0xe67e22,0x95a5a6,0x607d8b,0x979c9f,0x546e7a,0x7289da,0x99aab5))
     )
     await logch.send(embed=embed) 
-    
+
+# 60秒に一回ループ
+@tasks.loop(seconds=60)
+async def loop():
+    # 現在の時刻
+    now = datetime.datetime.now().strftime('%H:%M')
+    if now == '09:00':
+        channel = client.get_channel(CHANNEL_ID)
+        await channel.send('９：００です！おはようございます！今日も一日頑張りましょう！')  
+    elif now == '23:00':
+        channel = client.get_channel(CHANNEL_ID)
+        await channel.send('２３：００です！おやすみなさい！以降のメンションはお控え下さい。') 
+#ループ処理実行
+loop.start()
+  
 client.run(TOKEN)
 
 #ノア
